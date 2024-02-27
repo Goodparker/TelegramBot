c4571ddb248133419a425d01fa5314d099d5e809
Parker <63202704+Goodparker@users.noreply.github.com>
2024-02-27 23:35:12 +0500

    ЕЕЕ

---
+++ SpringDemoBot/src/main/java/io/proj3ct/SpringDemoBot/service/DocsQuickstart.java
@@ -0,0 +1,93 @@
+package io.proj3ct.SpringDemoBot.service;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.docs.v1.Docs;
+import com.google.api.services.docs.v1.DocsScopes;
+import com.google.api.services.docs.v1.model.Body;
+import com.google.api.services.docs.v1.model.Document;
+import com.google.api.services.docs.v1.model.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class DocsQuickstart {
+    private static final String APPLICATION_NAME = "Test";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "55c188a83546fc188e51576ba72836e0600e8b73";
+    private static final String DOCUMENT_ID = "1ea-nRoP4aNBSrAJ8Cm26gdcqq9fu_ltqAd7vV7gqHqk";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(DocsScopes.DOCUMENTS_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = DocsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+    //---------------------------------------------
+    private static void insertText(Docs service, String documentId, String text) throws IOException {
+        // Create a text paragraph
+        Paragraph paragraph = new Paragraph().setElements(Collections.singletonList(new TextRun().setText(text)));
+
+        // Create a request to insert the paragraph at the beginning of the document
+        InsertTextRequest insertTextRequest = new InsertTextRequest().setText(text).setEndOfSegmentLocation(new EndOfSegmentLocation());
+
+        // Execute the request to insert the paragraph
+        BatchUpdateDocumentRequest batchUpdateDocumentRequest = new BatchUpdateDocumentRequest().setRequests(
+                Collections.singletonList(new Request().setInsertText(insertTextRequest)));
+        service.documents().batchUpdate(documentId, batchUpdateDocumentRequest).execute();
+    }
+    //---------------------------------------------
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Docs service = new Docs.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Prints the title of the requested doc:
+        // https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+        Document response = service.documents().get(DOCUMENT_ID).execute();
+        String title = response.getTitle();
+        String textToInsert = "This is a new paragraph added to_ the document.";
+        insertText(service, DOCUMENT_ID, textToInsert);
+        System.out.printf("The title of the doc is: %s\n", title);
+    }
+}
