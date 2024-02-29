fba6e8a325c3bd94c66d75007e8c0099fc0f2318
Parker <63202704+Goodparker@users.noreply.github.com>
2024-03-01 00:59:24 +0500

    Update BotInicializer.java

--- SpringDemoBot/src/main/java/io/proj3ct/SpringDemoBot/config/BotInicializer.java
+++ SpringDemoBot/src/main/java/io/proj3ct/SpringDemoBot/config/BotInicializer.java
@@ -2,6 +2,7 @@ package io.proj3ct.SpringDemoBot.config;
 
 import com.google.api.services.docs.v1.Docs;
 import com.google.api.services.docs.v1.model.Document;
+import io.proj3ct.SpringDemoBot.service.DocsQuickstart;
 import io.proj3ct.SpringDemoBot.service.TelegramBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Component
 public class BotInicializer {
@@ -21,14 +25,18 @@ public class BotInicializer {
     public void init() throws TelegramApiException{
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
 
-
         try{
             telegramBotsApi.registerBot(bot);
+            DocsQuickstart.start();
 
         }
         catch (TelegramApiException e)
         {
 
+        } catch (GeneralSecurityException e) {
+           // throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
