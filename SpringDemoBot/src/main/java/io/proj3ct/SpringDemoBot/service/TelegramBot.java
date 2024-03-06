a615d668534f44531e11b3e9270d742afc10a376
Parker <63202704+Goodparker@users.noreply.github.com>
2024-03-07 00:15:53 +0500

    Update TelegramBot.java

--- SpringDemoBot/src/main/java/io/proj3ct/SpringDemoBot/service/TelegramBot.java
+++ SpringDemoBot/src/main/java/io/proj3ct/SpringDemoBot/service/TelegramBot.java
@@ -1,42 +1,31 @@
 package io.proj3ct.SpringDemoBot.service;
-
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.services.docs.v1.Docs;
-import com.google.api.services.docs.v1.model.BatchUpdateDocumentRequest;
-import com.google.api.services.docs.v1.model.InsertTextRequest;
-import com.google.api.services.docs.v1.model.Location;
-import com.google.api.services.docs.v1.model.Request;
-import com.vdurmont.emoji.EmojiParser;
 import io.proj3ct.SpringDemoBot.config.BotConfig;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+
+
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.IOException;
 import java.util.*;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot{
     final BotConfig config;
+    private  DocsQuickstart docsQuickstart;
     //---------------------------------------------------------------------------------------------
-    public TelegramBot(BotConfig config)
+    public TelegramBot(BotConfig config, DocsQuickstart docsQuickstart)
     {
         this.config = config;
+        this.docsQuickstart = docsQuickstart;
     }
     @Override
     public String getBotUsername() { return config.getBotName(); }
     @Override
     public String getBotToken() { return config.getBotToken(); }
-    String text;
+     public String text;
     //---------------------------------------------------------------------------------------------
     @Override
     public void onUpdateReceived(Update update) {
@@ -50,15 +39,16 @@ public class TelegramBot extends TelegramLongPollingBot{
                 String firstName = update.getMessage().getChat().getFirstName();
                 String lastName = update.getMessage().getChat().getLastName();
                 Date mytime = new Date();
-                 text = "Group: " + groupName + System.lineSeparator() +
+                text = "Group: " + groupName + System.lineSeparator() +
                         "Time: " + mytime + System.lineSeparator() +
                         "User: " + firstName + " " + lastName + System.lineSeparator() +
                         "Text: " + messageText;
 
                 System.out.println(text);
+                docsQuickstart.receivedTextFromTelegramBot(text);
+
             }
         }
     }
     //---------------------------------------------------------------------------------------------
 }
-//---------------------------------------------------------------------------------------------
